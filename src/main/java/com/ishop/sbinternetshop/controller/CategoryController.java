b237da5461af46cf0e7768d96205400af5d6a39c
rk800 <becreatative@gmail.com>
2025-07-31 14:10:48 +0500

    added delete mapping for Category, Category interface, business logic methods in CategoryServiceIm l

--- src/main/java/com/ishop/sbinternetshop/controller/CategoryController.java
+++ src/main/java/com/ishop/sbinternetshop/controller/CategoryController.java
@@ -1,10 +1,12 @@
 package com.ishop.sbinternetshop.controller;
 
 import com.ishop.sbinternetshop.model.Category;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.ishop.sbinternetshop.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,16 +14,27 @@ import java.util.List;
 @RestController
 public class CategoryController {
 
-    private List<Category> categories =  new ArrayList<>();
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping("/api/public/categories")
-    public List<Category> getAllCategories() {
-        return categories;
+    public List<Category> getAllCategories(){
+        return categoryService.getAllCategories();
     }
 
     @PostMapping("/api/public/categories")
     public String createCategory(@RequestBody Category category){
-        categories.add(category);
+        categoryService.createCategory(category);
         return "Category added successfully";
+    }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId){
+        try {
+            String status = categoryService.deleteCategory(categoryId);
+            return new ResponseEntity<>(status, HttpStatus.OK);
+        } catch (ResponseStatusException e){
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
     }
 }
--- src/main/java/com/ishop/sbinternetshop/service/CategoryService.java
+++ src/main/java/com/ishop/sbinternetshop/service/CategoryService.java
@@ -1,4 +1,12 @@
 package com.ishop.sbinternetshop.service;
 
-public class CategoryService {
+import com.ishop.sbinternetshop.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+    void createCategory(Category category);
+
+    String deleteCategory(Long categoryId);
 }
